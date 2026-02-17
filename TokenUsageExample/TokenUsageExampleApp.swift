4b596c423656b8ac822f255311f0e53850cb90ac
Artem Novichkov <mail@artemnovichkov.com>
2026-02-17 13:34:39 +0500

    Initial Commit

---
+++ TokenUsageExample/TokenUsageExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TokenUsageExampleApp.swift
+//  TokenUsageExample
+//
+//  Created by Artem Novichkov on 17.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct TokenUsageExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
